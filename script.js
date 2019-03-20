a978d56c80e00ede32e898c8a9fd8b0f5ccedb47
Protocol1228 <43751555+Protocol1228@users.noreply.github.com>
2019-03-20 09:50:32 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(".butt").click(function() {
-     var speech = (parseInt($(".results").val()));
-     $(".results").append(name);
+     var sport = $("#sport").val();
+     var movie = $("#movie").val();
+     var elaborate= $("#elaborate").val();
+      $(".results").append("In the year 2050 "+sport+"will be in "+movie+elaborate);
      });
